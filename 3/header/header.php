644440e3adf2733b704bb46b9447e31e624907b4
bemo <mohamed.bashandy.bemo@gmail.com>
2024-11-11 16:46:43 +0200

    header for challenge 3

---
+++ 3/header/header.php
@@ -0,0 +1,22 @@
+<header class="header">
+    <div class="right">
+        <div class="list-component">
+            <p class="hr1"></p>
+            <p class="hr2"></p>
+            <p class="hr3"></p>
+        </div>
+        <div>
+            <p class="back">◀&nbsp;&nbsp;&nbsp;Back</p>
+        </div>
+    </div>
+    <div class="center">
+        <p class="alpine">ALPINE</p>
+    </div>
+    <div class="left">
+        <span>Cart <span class="cart">(0)</span></span>
+        <select class="lang" name="lang" id="lang">
+            <option value="AR">AR</option>
+            <option value="EN" selected>EN</option>
+        </select>
+    </div>
+</header>
