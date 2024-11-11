2c56ffe83edff8b21b1c0f4d9dc3eb2d60832fb4
bemo <mohamed.bashandy.bemo@gmail.com>
2024-11-11 18:07:24 +0200

    end nav

--- 3/nav/nav.php
+++ 3/nav/nav.php
@@ -1,3 +1,17 @@
 <nav>
-    
+    <div class="container">
+        <div class="texts">
+            <h2>/ Sunglasses</h2>
+            <p>French know-how with high-technology<br> lenses in order to never be taken by <br>surprise when there is a sharp bright<br> sunny turn in the road.</p>
+            <button class = 'buy-it'>BUY IT ‹</button>
+        </div>
+        <div class="image-container">
+            <div class="image-background"></div>
+            <img src="images/sunglass.webp" alt="Image with changed background">
+        </div>
+    </div>
+    <div class='container'>
+        <p class="product-detials">▾ Product Detials </p>
+        <p class="page-layout"> 01 &nbsp;———&nbsp; 04 &nbsp;&nbsp;&nbsp;→</p>
+    </div>
 </nav>
